af801a55c08a5e48662768a44e79ed6cb0f8289a
mmergal92 <mmergal92@gmail.com>
2021-06-01 20:44:19 -0400

    update part 2

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Route, Switch } from "react-router-dom";
 // IMPORT PAGES
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Projects from "./pages/projects";
 
 function App() {
   // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
